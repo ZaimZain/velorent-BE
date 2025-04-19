eaeeb736c9ad0a56619a6cbb6b0f2a4eff261650
ZaimZain <zaim.shahiran@gmail.com>
2025-04-20 03:58:20 +0800

    hardcoded username and password as user

--- src/main/java/com/devion/velorent/controller/AuthController.java
+++ src/main/java/com/devion/velorent/controller/AuthController.java
@@ -1,41 +1,23 @@
 package com.devion.velorent.controller;
 
 import com.devion.velorent.entity.Role;
-import com.devion.velorent.entity.User;
 import com.devion.velorent.repository.UserRepository;
-import com.devion.velorent.security.JwtUtil;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping()
 public class AuthController {
     @Autowired
     private UserRepository userRepo;
 
-    @Autowired
-    private JwtUtil jwtUtil;
-
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody AuthRequest request) {
-        Optional<User> userOpt = userRepo.findByUsername(request.getUsername());
-        if (userOpt.isPresent()) {
-            User user = userOpt.get();
-            if (user.getPassword().equals(request.getPassword())) {
-                String token = jwtUtil.generateToken(user);
-                return ResponseEntity.ok(new AuthResponse(token, user.getRole()));
-            }
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+    @GetMapping("/hello")
+    public String login(){
+        return "Hello";
     }
 }
 
