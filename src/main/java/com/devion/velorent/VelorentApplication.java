f76d2adaaa437817245ace925d31156f28c5e4d8
ZaimZain <zaim.shahiran@gmail.com>
2025-04-20 01:57:10 +0800

    Pilot Project with MySQL Connection

---
+++ src/main/java/com/devion/velorent/VelorentApplication.java
@@ -0,0 +1,13 @@
+package com.devion.velorent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelorentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelorentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devion/velorent/VelorentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devion.velorent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelorentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
