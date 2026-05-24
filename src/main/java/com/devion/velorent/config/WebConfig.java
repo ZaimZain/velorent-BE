91818619c9df83a2d6f60491607aa4a6fdc20634
ZaimZain <zaim.shahiran@gmail.com>
2026-05-24 21:26:28 +0800

    Send Auth session when login and retrieving cars via UI

--- src/main/java/com/devion/velorent/config/WebConfig.java
+++ src/main/java/com/devion/velorent/config/WebConfig.java
@@ -10,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("*") // ✅ instead of allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
-            .allowCredentials(true);
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
